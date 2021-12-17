72248fcc8d9fe8c34faeae39b58243250019375b
Ruslan Khazbulatov <rkhazbulatov@ozon.ru>
2021-12-17 04:48:30 +0300

    :wrench: fix production build

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
-  future: {
-    webpack5:true,
-  }
+    reactStrictMode: true,
+    future: {
+        webpack5: true,
+    },
+    images: {
+        loader: 'akamai',
+        path: '',
+    },
+    assetPrefix: './',
 }
